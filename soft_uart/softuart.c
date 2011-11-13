2e1484ce57cbcaef225d54a2db67a6a6d587082f
Sebastian Trahm <basti@schleifi.com>
2011-11-13 18:47:50 +0100

    sync

--- soft_uart/softuart.c
+++ soft_uart/softuart.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2011 Sebastian Trahm
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
@@ -30,6 +47,15 @@ uartTransmitByte(unsigned char data)
 {
 	while (!(UCSR0A&(1 << UDRE0)));
 	UDR0 = data;
+}
+
+void
+uartPutString(const char *str)
+{
+	unsigned char c;
+
+	while ((c = *str++))
+		uartTransmitByte(c);
 }
 
 void
