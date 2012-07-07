a0eb3a1c73f4b3cd9465b1ed87ac5e314040445b
Sebastian Trahm <basti@schleifi.com>
2012-07-07 12:48:16 +0200

    Get rid of magic numbers and clarify names.

--- ds_1307/main.c
+++ ds_1307/main.c
@@ -28,36 +28,24 @@
 #include "../hw_uart/uart.h"
 #include "../sw_i2c/i2c.h"
 
-#define SECOND	1000
+#define BUFFER_SIZE	32
+#define SECOND		1000
 
 int
 main(void)
 {
 	static const char infostring[] PROGMEM = "RTC Demo - DS1307\r\n";
 	struct rtc_tm* rtc = NULL;
-	char buffer[32];
+	char buffer[BUFFER_SIZE];
 
-	/* set User LED on Port B as output */
 	DDRB = LED;
-	/* initialize UART */
 	uartInit();
-	/*
-	 * Initialize I2C Bus to Standard Mode.
-	 * According to the datasheet the DS1307 operates in
-	 * the standard mode (100kHz) only.
-	 * See section "I2C DATA BUS" on page 10 
-	 */
 	i2cInit(I2C_STD_MODE);
 	sei();
 
-	/* Enable DS1307 and SQW Output */
 	ds1307Init();
-	/* Initialize DS1307 with System Time */
 	ds1307SetTime(SYS_HOUR, SYS_MINS, SYS_SECS);
-	/* Initialize DS1307 with System Date */
 	ds1307SetDate(SYS_DAY, SYS_MONTH, SYS_YEAR);
-
-	/* output Startup Message on UART */
 	uartPutString_P(infostring);
 
 	while (1) {
