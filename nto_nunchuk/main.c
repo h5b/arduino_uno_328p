6862e60d25486bfcee8af7c21eff9c1f5437e24f
Sebastian Trahm <basti@schleifi.com>
2013-07-01 23:31:05 +0200

    Initial Calculation of Euler Angles.

---
+++ nto_nunchuk/main.c
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2013 Sebastian Trahm
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include <util/delay.h>
+
+#include "hw_uart/uart.h"
+#include "sw_i2c/i2c.h"
+#include "nunchuk.h"
+
+#define BUFFER_SIZE	80
+#define BAUDRATE	115200
+
+#define ACCEL_CALIB_X	500
+#define ACCEL_CALIB_Y	500
+#define ACCEL_CALIB_Z	500
+
+#define WEIGHTING	0.0625
+
+volatile int old_ax = 0;
+volatile int old_ay = 0;
+volatile int old_az = 0;
+
+/* angles */
+static inline int
+nunchukGetRoll(struct nunchuk_tm* v)
+{
+	int ax = v->accelX - ACCEL_CALIB_X;
+	int az = v->accelZ - ACCEL_CALIB_Z;
+
+	/* simple low pass filter */
+	ax = (ax * WEIGHTING) + (old_ax * (1.0 - WEIGHTING));
+	az = (az * WEIGHTING) + (old_az * (1.0 - WEIGHTING));
+
+	old_ax = ax;
+	old_az = az;
+
+	return (int) (atan2((double)ax, (double)az) * 180 / M_PI);
+}
+
+static inline int
+nunchukGetPitch(struct nunchuk_tm* v)
+{
+	int ay = v->accelY - ACCEL_CALIB_Y;
+	int az = v->accelZ - ACCEL_CALIB_Z;
+
+	ay = (ay * WEIGHTING) + (old_ay * (1.0 - WEIGHTING));
+	az = (az * WEIGHTING) + (old_az * (1.0 - WEIGHTING));
+
+	old_ay = ay;
+	old_az = az;
+
+	return (int) (atan2((double)ay, (double)az) * 180 / M_PI);
+}
+
+int
+main(void)
+{
+	struct nunchuk_tm* nunchuk = NULL;
+	char buffer[BUFFER_SIZE];
+	int roll = 0;
+	int pitch = 0;
+
+	i2cInit(I2C_STD_MODE);
+	uartInit(BAUDRATE);
+	sei();
+
+	nunchukInit();
+
+	/* initial read */
+	nunchuk = nunchukGetData();
+	old_ax = nunchuk->accelX;
+	old_ay = nunchuk->accelY;
+	old_az = nunchuk->accelZ;
+
+
+	while (1) {
+		nunchuk = nunchukGetData();
+		roll = nunchukGetRoll(nunchuk);
+		pitch = nunchukGetPitch(nunchuk);
+
+		sprintf(buffer,
+		    "x@%03d,y@%03d,Roll@%04d,Pitch@%04d,c@%d,z@%d\r\n",
+		    nunchuk->joystickX,
+		    nunchuk->joystickY,
+		    roll,
+		    pitch,
+		    nunchuk->buttonC,
+		    nunchuk->buttonZ
+		);
+		uartPutString(buffer);
+		_delay_ms(200);
+	}
+
+	/* never reached */
+	return 0;
+}
---
+++ nto_nunchuk/nunchuk.h
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2013 Sebastian Trahm
+ * All rights reserved.
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF MIND, USE, DATA OR PROFITS, WHETHER IN
+ * AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#ifndef _NUNCHUK_H_
+#define _NUNCHUK_H_
+
+/*
+ * Wii Nunchuk - Circuitry
+ *
+ *     ^
+ *     |       ,__________.
+ *     '---01--|GND    GND|--12-----[WH]---,     ,--------------------------.
+ *       --02--|B1      A1|--11--           \____|                          |
+ *  ,------03--|B2      A2|--10-----[YE]---------|                          |
+ *  | ,----04--|B3      A3|--09-----[GN]---------|                          |
+ *  | |  --05--|B4      A4|--08--          ,-----|                          |
+ *  | | ,--06--|5V     3V3|--07-----[RS]---'     `--------------------------'
+ *  | | |      `----------'                        Wii Nunchuk
+ *  | | |         BSS138
+ *  | | |
+ *  | | |
+ *  | | `--------------> 5V
+ *  | |
+ *  | `---------[SDA]--> PC4
+ *  `-----------[SCL]--> PC5
+ */
+
+/*
+ * Register Map and Protocol Information by WiiBrew (16/09/2012)
+ * http://wiibrew.org/wiki/Wiimote/Extension_Controllers/Nunchuck
+ */
+
+/*
+ * Wii Nunchuk - Register Map
+ *
+ *       ,-------------------------------------.
+ *  0x00 |        X Position (Joystick)        |
+ *       |-------------------------------------|
+ *  0x01 |        Y Position (Joystick)        |
+ *       |-------------------------------------|
+ *  0x02 |        X Acceleration (Gyro)        | Bit 9..2
+ *       |-------------------------------------|
+ *  0x03 |        Y Acceleration (Gyro)        | Bit 9..2
+ *       |-------------------------------------|
+ *  0x04 |        Z Acceleration (Gyro)        | Bit 9..2
+ *       |---------,---------,---------,---,---|
+ *  0x05 | Z (1..0)| Y (1..0)| X (1..0)| C | Z |
+ *       `---------'---------'---------'---'---'
+ *       7                                     0
+ */
+
+#define NUNCHUK_BTN_C_FLAG	0x02
+#define NUNCHUK_BTN_Z_FLAG	0x01
+#define NUNCHUK_I2C_MSDELAY	10
+#define NUNCHUK_REG_START	0x00
+#define NUNCHUK_REG_SIZE	0x06
+
+#define NUNCHUK_RD_ADDR		0xA5
+#define NUNCHUK_WR_ADDR		0xA4
+
+/* Normalized Nunchuk Sensor Data */
+struct nunchuk_tm {
+	uint8_t joystickX;
+	uint8_t joystickY;
+	uint16_t accelX; /* MSB and LSB of 5th Byte */
+	uint16_t accelY;
+	uint16_t accelZ;
+	uint8_t buttonZ;
+	uint8_t buttonC;
+};
+
+extern struct nunchuk_tm nunchukData;
+
+struct nunchuk_tm* nunchukGetData(void);
+void nunchukInit(void);
+
+#endif /* _NUNCHUK_H_ */
