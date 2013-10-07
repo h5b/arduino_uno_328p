489242f7453e7b64dc2068b3782226ab8c85130d
Sebastian Trahm <basti@schleifi.com>
2013-10-07 18:15:08 +0200

    Add USB Support to enable AVRISP_MKII on VM.

--- cookbooks/avr/recipes/default.rb
+++ cookbooks/avr/recipes/default.rb
@@ -8,6 +8,33 @@
   "check",
   "gcc-avr",
   "gdb-avr",
-]. each do |p|
+].each do |p|
   package p
+end
+
+if node['udev_avr']
+  execute 'vagrant_to_group_dialout' do
+    command "adduser vagrant dialout"
+  end
+
+  Chef::Log.info(node['udev_subsys'])
+  avr_ispmk2 = node['udev_avr']['udev_subsys']
+  avr_ispmk2 += "\n"
+  avr_ispmk2 += node['udev_avr']['udev_attrib']
+  avr_ispmk2 += "\n"
+  avr_ispmk2 += node['udev_avr']['udev_label']
+  avr_ispmk2 += "\n"
+  file "/etc/udev/rules.d/10-avrisp.rules" do
+    owner "root"
+    group "root"
+    content avr_ispmk2
+    action :create
+  end
+
+else
+  Chef::Log.info("No udev_avr found.")
+  file "/etc/udev/rules.d/10-avrisp.rules" do
+    action :delete
+    only_if {::File.exists?("/etc/udev/rules.d/10-avrisp.rules")}
+  end
 end
